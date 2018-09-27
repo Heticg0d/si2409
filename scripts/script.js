05383efb112727446f525b67fcdb506c00b3a7f2
Heticg0d <maxime.cassagne@gmail.com>
2018-09-27 15:37:49 +0200

    add trans iph
    add trans iph+js

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 $(window).scroll(function(){
-  $("#particles-js").css({"margin-top": -0.8($(window).scrollTop()) + "px"});
+  $("#particles-js").css({"margin-top": -($(window).scrollTop()) + "px"});
 });
 
 $(window).scroll(function(){
@@ -18,3 +18,41 @@ $(window).scroll(function(){
 $(window).scroll(function(){
     $(".logo").css("opacity", 0 + $(window).scrollTop() / 250);
   });
+
+
+//for the first iphone to move in a stick/destick way
+
+function inputcallback()
+{
+	var iphonepic = document.getElementById("iphone_container");
+	var sticky = iphonepic.offsetTop;
+
+}
+
+window.onscroll = function() {
+	myFunction()};
+
+var iphonepic = document.getElementById("iphone_container");
+var sticky = iphonepic.offsetTop;
+
+function myFunction() 
+{
+  if (window.pageYOffset > sticky -100) 
+  {
+    if (window.pageYOffset > sticky +900)
+    {
+
+    	iphonepic.style.animation = "opa-out 2s infinite";
+    	iphonepic.classList.remove("sticky");
+    }
+    else
+    {
+    	iphonepic.classList.add("sticky");
+    }
+
+  } 
+  else 
+  {
+    iphonepic.classList.remove("sticky");
+  }
+}
