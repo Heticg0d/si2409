55a644e3aae3fec3216fad57a976ef9ee70236c8
Tichyus <32875615+Tichyus@users.noreply.github.com>
2018-09-27 15:42:41 +0200

    header appear

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,33 @@
-$(window).scroll(function(){
-  $("#particles-js").css({"margin-top": -0.8($(window).scrollTop()) + "px"});
+var mySVG = $('#animated_logo_dryp').drawsvg({
+  duration : 2000
+});
+mySVG.drawsvg('animate');
+
+
+
+
+//if (document.pageYOffset > 10) {
+//  console.log('hello world')
+//}
+
+let body = document
+let total_height =
+
+$(window).on("scroll", function() {
+    if($(window).scrollTop() > ) {
+        header[0].classList.add("appear");
+    } else {
+       header[0].classList.remove("appear");
+    }
 });
 
-$(window).scroll(function(){
-  $("article").css({"margin-top": -1.2*($(window).scrollTop()) + "px"});
+
+let header = document.getElementsByClassName("header")
+
+$(window).on("scroll", function() {
+    if($(window).scrollTop() > 10) {
+        header[0].classList.add("appear");
+    } else {
+       header[0].classList.remove("appear");
+    }
 });
-
-
-$(window).scroll(function(){
-    $(".texto").css("opacity", 1 - $(window).scrollTop() / 250);
-  });
-
-$(window).scroll(function(){
-    $("article").css("opacity", 0 + $(window).scrollTop() / 250);
-  });
-
-$(window).scroll(function(){
-    $(".logo").css("opacity", 0 + $(window).scrollTop() / 250);
-  });
